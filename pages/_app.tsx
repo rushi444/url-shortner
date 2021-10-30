4290619f8f40e9e11aa8696a9b1b660c8bd8b430
rushi <rushi44@gmail.com>
2021-10-29 19:57:34 -0700

    chakra ui + some styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider resetCSS>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import React, { useState } from 'react'
+import {
+  Container,
+  Heading,
+  Text,
+  Button,
+  Input,
+  Box,
+  HStack,
+  Center
+} from '@chakra-ui/react'
 
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  const [value, setValue] = useState('https://www.rushicodes.com/')
+  const [value, setValue] = useState('')
   const [shortUrl, setShortUrl] = useState<string>()
 
   const onSubmit = async (e: React.FormEvent) => {
@@ -20,17 +30,29 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div>
-      {shortUrl ? (
-        <div>
-          <a href={shortUrl}>{shortUrl}</a>
-        </div>
-      ) : null}
-      <form onSubmit={onSubmit}>
-        <input value={value} onChange={e => setValue(e.target.value)} />
-        <button type="submit">Shorten!</button>
-      </form>
-    </div>
+    <Center h="100vh">
+      <Box>
+        <Heading as="h1">
+          Shorten URLs on{' '}
+          <Text as="span" color="teal.200">
+            the Edge
+          </Text>
+        </Heading>
+        <form onSubmit={onSubmit}>
+          <HStack>
+            <Input value={value} onChange={e => setValue(e.target.value)} />
+            <Button type="submit" colorScheme="teal">
+              Shorten
+            </Button>
+          </HStack>
+        </form>
+        {shortUrl ? (
+          <Box>
+            <a href={shortUrl}>{shortUrl}</a>
+          </Box>
+        ) : null}
+      </Box>
+    </Center>
   )
 }
 
